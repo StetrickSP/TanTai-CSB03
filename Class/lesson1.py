4bc11d5f4bf3395f4fa6cc027afc080e173dbbe7
StetrickSP <141539538+StetrickSP@users.noreply.github.com>
2024-03-31 11:54:05 +0700

    Add files via upload

---
+++ Class/lesson1.py
@@ -0,0 +1,31 @@
+# a = 5
+# b = 3
+# sum = a + b
+# print('Hello, World!')
+# print(sum)
+# if a > b:
+#     print("A is greater than B")
+# else:
+#     print("A is smaller than B")
+from turtle import *
+pen
+# Square
+# forward(90)
+# right(90)
+# forward(90)
+# right(90)
+# forward(90)
+# right(90)
+# forward(90)
+# Pentagon
+left(180-36)
+forward(90)
+left(180-36)
+forward(90)
+left(180-36)
+forward(90)
+left(180-36)
+forward(90)
+left(180-36)
+forward(90)
+mainloop()
